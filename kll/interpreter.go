cc576c78fe17d7058f87e375ad161b35e621022a
kaklikOf13 <canaldokaklik@gmail.com>
2024-05-31 21:05:29 -0300

    refactor:Preparing to 0.1

--- kll/interpreter.go
+++ kll/interpreter.go
@@ -1,6 +1,9 @@
 package kll
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type Interpreter struct {
 	Scope *Scope
@@ -84,26 +87,46 @@ func (inter *Interpreter) Exec_Node(node Node, expected_type Type, callstack []C
 	}
 	return nil, Success
 }
-func (inter *Interpreter) Eval(code string) Value {
+func (inter *Interpreter) Log(txt ...any) {
+	fmt.Println(txt...)
+}
+func (inter *Interpreter) Panic(err Error) {
+	if !err.Is(Success) {
+		inter.Log(err.String())
+		os.Exit(1)
+	}
+}
+func (inter *Interpreter) Eval(code string, all_lines bool) ([]Value, Error) {
 	nodes, err := Parse(code)
 	if !err.Is(Success) {
 		fmt.Println(err)
 	}
+	ret := []Value{}
 	for i := range nodes {
-		if i == len(nodes)-1 {
+		if !all_lines && i == len(nodes)-1 {
 			scop := inter.Scope.AddChild("eval")
 			val, err := inter.Exec_Node(nodes[i], nil, []Callstack{}, scop)
 			inter.Scope.RemoveChild(scop.Name)
 			if !err.Is(Success) {
-				fmt.Println(err)
-				return nil
+				return nil, err
 			}
-			return val
+			return []Value{val}, Success
 		} else {
-			inter.Exec_Node(nodes[i], nil, []Callstack{}, inter.Scope)
+			var v Value
+			v, err = inter.Exec_Node(nodes[i], nil, []Callstack{}, inter.Scope)
+			if !err.Is(Success) {
+				return nil, err
+			}
+			if all_lines {
+				ret = append(ret, v)
+			}
 		}
 	}
-	return Null
+	return ret, Success
+}
+func (inter *Interpreter) Exec(code string) Error {
+	_, err := inter.Eval(code, false)
+	return err
 }
 
 func NewInterpreter(scope *Scope) *Interpreter {
--- kll/test/interpreter_test.go
+++ kll/test/interpreter_test.go
@@ -9,18 +9,38 @@ import (
 
 func TestInterpreterSum(t *testing.T) {
 	inter := kll.NewInterpreter(kll.NewScope())
-	fmt.Println(inter.Eval("11+13"))
+	val, err := inter.Eval("11+13", false)
+	inter.Panic(err)
+	fmt.Println(val)
+	if !val[0].Is(kll.Int.Create(24)) {
+		t.Fail()
+	}
 }
 
 func TestInterpreterSub(t *testing.T) {
 	inter := kll.NewInterpreter(kll.NewScope())
-	fmt.Println(inter.Eval("15-13"))
+	val, err := inter.Eval("15-13", false)
+	inter.Panic(err)
+	fmt.Println(val)
+	if !val[0].Is(kll.Int.Create(2)) {
+		t.Fail()
+	}
 }
 func TestInterpreterMult(t *testing.T) {
 	inter := kll.NewInterpreter(kll.NewScope())
-	fmt.Println(inter.Eval("15*2"))
+	val, err := inter.Eval("15*2", false)
+	inter.Panic(err)
+	fmt.Println(val)
+	if !val[0].Is(kll.Int.Create(30)) {
+		t.Fail()
+	}
 }
 func TestInterpreterDiv(t *testing.T) {
 	inter := kll.NewInterpreter(kll.NewScope())
-	fmt.Println(inter.Eval("50/2"))
+	val, err := inter.Eval("50/2", false)
+	inter.Panic(err)
+	fmt.Println(val)
+	if !val[0].Is(kll.Int.Create(25)) {
+		t.Fail()
+	}
 }
